a487cfa5b9bad409778181e5b30a3d4eabcce891
Doeunnkimm <doeunnkimm@gmail.com>
2023-03-23 01:56:26 +0900

    :ambulance: Fix : 빠진 import로 생긴 오류 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GlobalStyles from './Styles/global'
 import theme from './Styles/theme'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/routing'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
 	const queryClient = new QueryClient()
