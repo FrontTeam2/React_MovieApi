98b07573683a56e231771c0673c7d3a00c49a327
beenbin <77373566+showme0241@users.noreply.github.com>
2023-03-23 00:03:33 +0900

    :lipstick: Design : 전역 스타일 파일 수정

--- src/Styles/global.js
+++ src/Styles/global.js
@@ -3,10 +3,44 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
+    margin:0;
+    padding:0;
   }
 
   body {
     font-family: 'Noto Sans KR', sans-serif;
+  }
+
+  html {
+    font-size:62.5%
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  h1 {
+    font-size:${({ theme }) => theme.FONT_SIZE.medium}
+  }
+
+  h2 {
+    font-size:${({ theme }) => theme.FONT_SIZE.huge}
+  }
+
+  h2 + p {
+    font-size:${({ theme }) => theme.FONT_SIZE.medium}
+  }
+
+  h3 {
+    font-size:${({ theme }) => theme.FONT_SIZE.medium}
+  }
+
+  h4 {
+    font-size:${({ theme }) => theme.FONT_SIZE.small}
+  }
+
+  h4 + p {
+    font-size:${({ theme }) => theme.FONT_SIZE.tiny}
   }
 
   button {
