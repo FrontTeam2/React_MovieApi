4f5abca686854d65d406d9fab13a6cb823dde04f
Doeunnkimm <doeunnkimm@gmail.com>
2023-03-23 01:47:37 +0900

    :sparkles: Feat : api모듈화, query 훅 함수 정의
    모든 params는 객체로 전달해야 합니다.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import GlobalStyles from './Styles/global'
 import theme from './Styles/theme'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/routing'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+	const queryClient = new QueryClient()
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyles />
-			<RouterProvider router={router} />
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<GlobalStyles />
+				<RouterProvider router={router} />
+			</ThemeProvider>
+		</QueryClientProvider>
 	)
 }
 
