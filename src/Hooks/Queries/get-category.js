5fed1877ca9e5d449fe1b9b1394b6835f8665597
Doeunnkimm <doeunnkimm@gmail.com>
2023-03-23 14:25:06 +0900

    :ambulance: Fix : 잘못된 함수명으로 생긴 오류 수정

--- src/Hooks/Queries/get-category.js
+++ src/Hooks/Queries/get-category.js
@@ -4,7 +4,7 @@ import { QUERY_KEY } from '../../Consts/query-key'
 import { queryConfig } from './@config'
 
 const getCategory = async params => {
-	const res = await MovieApi.getPopular(params)
+	const res = await MovieApi.getCategory(params)
 	return res.data
 }
 
