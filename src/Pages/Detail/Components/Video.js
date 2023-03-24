d8723b699725aa2c5fc0f213a0888b461fb0d6f5
beenbin <77373566+showme0241@users.noreply.github.com>
2023-03-24 23:58:02 +0900

    :sparkles: Design : 상세 페이지 > 비디오 슬라이더 파일 추가

--- src/Pages/Detail/Components/Video.js
+++ src/Pages/Detail/Components/Video.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { MarginAuto } from '../../../Styles/common'
+import { FlexAlignCSS, MarginAuto } from '../../../Styles/common'
 
 function VideoSection() {
 	return (
@@ -8,7 +8,19 @@ function VideoSection() {
 				<h3>영상</h3>
 			</S.TitleText>
 			<S.Hr />
-			<S.Video></S.Video>
+			<S.VideoSliderWrap>
+				<S.VideoSlider>
+					<li>
+						<S.Video></S.Video>
+					</li>
+					<li>
+						<S.Video></S.Video>
+					</li>
+					<li>
+						<S.Video></S.Video>
+					</li>
+				</S.VideoSlider>
+			</S.VideoSliderWrap>
 		</S.VideoContainer>
 	)
 }
@@ -30,17 +42,41 @@ const Hr = styled.hr`
 	width: 100%;
 	margin: 1rem 0 4rem;
 `
+const VideoSliderWrap = styled.div`
+	height: 100%;
+`
+
+const VideoSlider = styled.ul`
+	${FlexAlignCSS}
+	height:100%;
+	padding-right: 2rem;
+	overflow-x: auto;
+	overflow-y: hidden;
+
+	& > li {
+		flex-shrink: 0;
+		margin-right: 2rem;
+		width: 95%;
+		height: 36rem;
+		background: var(--color-light-gray);
+		border-radius: 2rem;
+	}
+
+	& > li:last-child {
+		margin-right: 0;
+	}
+`
 
 const Video = styled.video`
 	width: 100%;
-	height: 36rem;
-	background: var(--color-light-gray);
-	border-radius: 2rem;
+	height: 100%;
 `
 
 const S = {
 	VideoContainer,
 	TitleText,
 	Hr,
+	VideoSliderWrap,
+	VideoSlider,
 	Video,
 }
